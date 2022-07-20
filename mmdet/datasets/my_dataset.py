0f176b0a1016fd863b42722b31164678b0dae3bc
mahnoor-amin <93726012+mahnoor-amin@users.noreply.github.com>
2022-07-20 12:48:18 +0500

    Update my_dataset.py

--- mmdet/datasets/my_dataset.py
+++ mmdet/datasets/my_dataset.py
@@ -3,7 +3,8 @@ from .registry import DATASETS
 from .xml_style import XMLDataset
 from .voc import VOCDataset
 @DATASETS.register_module
-class MyDataset(XMLDataset):
+class MyDataset(CocoDataset):
 
     # CLASSES =('Dent', 'Scratch_or_spot')#, 'Shatter', 'Tear')
-    CLASSES = ('RBC')
+    CLASSES = ("gametocyte", "schizont", "trophozoite", "ring")
+
